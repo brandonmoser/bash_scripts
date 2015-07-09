ea9baa7e59c38b76f0959af30bcdd327b6f3edf9
Brandon Moser <brandon@brandonmoser.com>
2015-07-08 23:18:15 -0500

    Create Spotify script

---
+++ install_spotify.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+clear
+
+# Make sure only root can run this script
+if [[ $EUID -ne 0 ]]; then
+   echo "This script must be run as root" 1>&2
+   exit 1
+fi
+
+# 1. Add the Spotify repository signing key to be able to verify downloaded packages
+sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys D2C19886
+
+# 2. Add the Spotify repository
+echo deb http://repository.spotify.com stable non-free | sudo tee /etc/apt/sources.list.d/spotify.list
+
+# 3. Update list of available packages
+sudo apt-get update
+
+# 4. Install Spotify
+sudo apt-get install spotify-client --reinstall
