7ca499581af08e2f5c10fe5522d9349c474e5569
Brandon Moser <brandon@brandonmoser.com>
2015-07-09 01:04:43 -0500

    Create Geary install script

---
+++ install_geary.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+clear
+
+# Make sure only root can run this script
+if [[ $EUID -ne 0 ]]; then
+   echo "This script must be run as root" 1>&2
+   exit 1
+fi
+
+# 1. Add Yorba PPA
+sudo add-apt-repository ppa:yorba/ppa
+
+# 2. Update Apt Repos
+sudo apt-get update
+
+# 3. Install Geary
+sudo apt-get install geary
