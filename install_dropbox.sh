f28763f731cf28b3f2b1f0d5081368fc7aef7a1f
Brandon Moser <brandon@brandonmoser.com>
2015-07-09 00:50:23 -0500

    Create Dropbox install script. Add Dropbox python control script

---
+++ install_dropbox.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+# Download the Linux x64 version
+cd ~ && wget -O - "https://www.dropbox.com/download?plat=lnx.x86_64" | tar xzf -
+
+# Run the daemon
+~/.dropbox-dist/dropboxd &
+
+# Echo that a control script is available
+echo "=============================================="
+echo "A control script is available here"
+echo "$ python dropbox.py"
+echo "with no arguments, usage will be displayed."
+echo "=============================================="
