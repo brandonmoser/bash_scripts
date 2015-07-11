fdfa799c9e936e1748e47e648f255c820b886362
Brandon Moser <brandon@brandonmoser.com>
2015-07-10 23:07:26 -0500

    Add ST3 install script. Closes #7

---
+++ install_sublimetext3.sh
@@ -0,0 +1,52 @@
+#!/usr/bin/env bash
+clear
+
+BUILD="3083"
+DEB_PKG="sublime-text_build-$BUILD_amd64.deb"
+DOWNLOAD_LINK="http://c758482.r82.cf2.rackcdn.com/$DEB_PKG"
+DOWNLOAD_DIR="/home/brandonmoser/Downloads"
+SCRIPTS_DIR="$(pwd)"
+
+# Make sure only root can run this script
+if [[ $EUID -ne 0 ]]; then
+   echo "This script must be run as root" 1>&2
+   exit 1
+fi
+
+download_deb_pkg() {
+    echo "-------------------------------------------"
+    cd $DOWNLOAD_DIR
+    wget $DOWNLOAD_LINK -O $DEB_PKG
+    echo ""
+}
+
+install_application() {
+    echo "Extracting... "
+    echo "-------------------------------------------"
+    cd $DOWNLOAD_DIR
+    sudo dpkg -iE $DEB_PKG
+    echo ""
+}
+
+fix_dependencies() {
+    echo "Updating Dependencies "
+    echo "-------------------------------------------"
+    sudo apt-get --yes --fix-broken install
+    echo ""
+}
+
+echo "===================================================="
+echo "| Installing Sublime Text 3 build $BUILD           |"
+echo "===================================================="
+echo ""
+if [ ! -f "$DEB_PKG" ]; then
+    echo "Downloading to $DOWNLOAD_DIR "
+    download_deb_pkg
+else
+    echo "$DEB_PKG exists, skipping downloads "
+    echo ""
+fi
+install_application
+fix_dependencies
+echo "DONE"
+exit 0
